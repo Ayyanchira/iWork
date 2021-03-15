625525032ecb7f570e01e6756cc078d5c6b9fca3
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2021-03-15 09:39:14 -0700

    Initial Commit

---
+++ iWork/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iWork
+//
+//  Created by Akshay Ayyanchira on 3/15/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
